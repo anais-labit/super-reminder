2384d0576ab4a40366b553c9bece047dd597ef4f
anais-labit <anais.labit@gmail.com>
2023-09-19 19:44:49 +0200

    header avec les lists

--- View/includes/header.php
+++ View/includes/header.php
@@ -3,31 +3,32 @@
 use App\Controllers\UserController;
 
 $role = new UserController();
-
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="index.php">Faux Site</a>
-        <div style="display: block; text-align:right">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Accueil</a>
-                </li>
+        <a class="navbar-brand" href="index.php">Super-Reminder</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto flex-column">
                 <li class="nav-item">
                     <?php if (isset($_SESSION['user'])) : ?>
                         <?php if ($role->validateAdminRole()) : ?>
                             <a class="nav-link" href="./admin.php">Admin</a>
+
                         <?php else : ?>
-                            <a class="nav-link" href="./profil.php">Profil</a>
+                            <a class="nav-link" href="./lists.php">Lists</a>
+                            <a class="nav-link" href="./profil.php">Profile</a>
                         <?php endif; ?>
                     <?php else : ?>
-                        <a class="nav-link" href="./connexion.php">Connexion</a>
+                        <a class="nav-link" href="./connexion.php">Sign In</a>
                     <?php endif; ?>
                 </li>
                 <li id="logOut" class="nav-item">
                     <?php if (isset($_SESSION['user'])) : ?>
-                        <a class="nav-link" href="index.php?logOut">Déconnexion</a>
+                        <a class="nav-link" href="index.php?logOut">Log Out</a>
                     <?php endif; ?>
                 </li>
             </ul>
